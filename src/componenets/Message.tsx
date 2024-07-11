f5d6323404cc9ad16fc9cf9c9fcc3cdf6fba3fe6
DaSolHan <9511dasole@gmail.com>
2024-07-11 19:17:55 +0900

    anim

--- src/componenets/Message.tsx
+++ src/componenets/Message.tsx
@@ -4,9 +4,10 @@ import { useNavigate } from "react-router-dom";
 function Message() {
     const navigate = useNavigate();
     const [state, setState] = useState("공사중");
-    alert(state)
-    if("공사중" === state){
-        navigate("/");
+    if ("공사중" === state) {
+        alert(state)
+        console.log("first")
+        navigate("/home");
     }
     return (
         <div>공사중</div>
